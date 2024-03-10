16310b1f8f4b98c23949672d389e4577daa30776
Yang Meng <54854973+yangmeng96@users.noreply.github.com>
2024-03-09 21:38:26 -0800

    roc update

--- roc.py
+++ roc.py
@@ -4,34 +4,28 @@ import matplotlib.pyplot as plt
 import os
 
 
-def ROC(Y_test, Y_scores, method, LR = True):
-    file_path = 'ROCfigs/'
+def ROC(Y_test, Y_scores, method, clf, seed):
+    file_path = 'figs/roc/'
     directory = os.path.dirname(file_path)
     if not os.path.exists(directory):
         os.makedirs(directory)
     
-    if LR == True:
-       type = "logistic regression"
-    elif LR == False:
-       type = "random forest"
-    
-    
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=(6, 4))
 
     for i, preds in enumerate(Y_scores):
       fpr, tpr, _= roc_curve(Y_test, preds)
       roc_auc = auc(fpr, tpr)
-      plt.plot(fpr, tpr, label=f'{method[i]} (AUC = {roc_auc:.2f})')
-
-   
-    # plt.plot([0, 1], [0, 1], 'k--', label='Random (AUC = 0.50)')
+      plt.plot(fpr, tpr, label=f'{method[i]} (AUC = {roc_auc:.3f})')
 
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
-    plt.title('Receiver Operating Characteristic'+type)
-    plt.legend(loc="lower right")
-    
+    if clf == "LR":
+        plt.title('ROC - Logistic Regression')
+    elif clf == "RF":
+        plt.title('ROC - Random Forest')
 
-    #plt.savefig(file_path + type + "_" + iter + ".png") 
+    plt.legend(loc="lower right")
+    print(f'Seed: {seed}')
+    plt.savefig(f'{file_path}{clf}_{seed}.png') 
     plt.show()
 
